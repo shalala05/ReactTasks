b8435109ee5b94c4aa7b6155655b3142825453cf
shalala05 <allahverdiyevashelale05@gmail.com>
2024-11-23 19:35:03 +0400

    firstcommit

---
+++ task1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './components/Button';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Button Example</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default App;
